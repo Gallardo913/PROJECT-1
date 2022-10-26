1524f67c1b5f0773fd54e1908ab8f903202925fd
Roger gallardo <rogergallardofs1@gmail.com>
2022-10-26 00:52:15 -0400

    third commit

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-$(function() {
+fetch('https://soccer.sportmonks.com/api/v2.0/players/search/', { mode: 'no-cors'})
+    .then(res => { 
+        res.json()) {
+            
+        }
+    .then(data => console.log(data))
 
-const promise = $.ajax({
-    url: 'https://live-score-api.com'
-});
 
-promise.then(
-    (data) => {
-        console.log(data)
-        render(data.results)
-    },
-    (error) => {
-        console.error(error)
-    }
-})
+
+
+
+
+
+
+// let apiName = "https://soccer.sportmonks.com"
+// let apiEndpoint = "/api/v2.0/players/search/"
+
+// let apiKey = "?api_token=8FKQiL93MuRlY10vDJJnAaxhvJ7QozKs5e2Jmq7dpN3agCgxPPrpT6zcH5Vw"
+
+// fetchfunction(apiName + apiEndpoint + apiKey)
+
+// async function fetchfunction(url)
+
+// console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+
+// const promise = $.ajax({
+//     url: 'https://soccer.sportmonks.com/api/v2.0/players/search/'
+// });
+
+// promise.then(
+//     (data) => {
+//         console.log(data)
+//         render(data.results)
+//     },
+//     (error) => {
+//         console.error(error)
+//     }
+// })
