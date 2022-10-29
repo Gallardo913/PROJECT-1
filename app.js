6de135efe7c6e34447133fbd66f76f01c7cb03a5
Roger gallardo <rogergallardofs1@gmail.com>
2022-10-29 02:08:03 -0400

    Final commit

--- app.js
+++ app.js
@@ -1,53 +1,79 @@
-fetch('https://soccer.sportmonks.com/api/v2.0/players/search/', { mode: 'no-cors'})
-    .then(res => { 
-        res.json()) {
-            
+const state = {
+    player1: 0,
+    player2: 0,
+    currentQuestion: {},
+    which: true
+}
+
+let questions = []
+
+//******************** */
+// Main DOM Element
+//******************* */
+
+const $question = $("#question")
+const $a = $("#a")
+const $b = $("#b")
+const $c = $("#c")
+const $d = $("#d")
+const $p1score = $("#player1 h4")
+const $p2score = $("#player2 h4")
+
+//******************** */
+// Functions
+//******************* */
+
+const chooseAnswer = (event, question) => {
+    console.log(event)
+    if(event.target.innerText === question.answer){
+        console.log("correct")
+        if (state.which){
+            state.player1++
+            state.which = !state.which
+        }else{
+            state.player2++
+            state.which = !state.which
         }
-    .then(data => console.log(data))
+        setBoard(questions)
+    } else {
+        console.log("incorrect")
+        setBoard(questions)
+        state.which = !state.which
+    }
+}
+
+const setBoard = (q) => {
+    // Getting a random question
+    const randomIndex = Math.floor(Math.random() * q.length)
+    const randomQuestion = q[randomIndex]
+
+    //Update question
+    $question.text(randomQuestion.question)
+    $a.text(randomQuestion.a)
+    $b.text(randomQuestion.b)
+    $c.text(randomQuestion.c)
+    $d.text(randomQuestion.d)
+
+    //update players scores
+    $p1score.text(state.player1) 
+    $p2score.text(state.player2)
+
+    $("li").off()
+    $("li").on("click", (event) => {
+        chooseAnswer(event, randomQuestion)
+    })
+}
 
 
 
 
 
+const URL = "https://cdn.contentful.com/spaces/2floofyjvstl/environments/master/entries?access_token=sNNQmYDewtD8Dbey02mqO_qwvJ6mM623XE2rETIMm9s"
+$.ajax(URL)
+.then((data) => {
+    questions = data.items.map((q) => q.fields)
+    console.log(data)
+    console.log(questions)
 
-
-
-// let apiName = "https://soccer.sportmonks.com"
-// let apiEndpoint = "/api/v2.0/players/search/"
-
-// let apiKey = "?api_token=8FKQiL93MuRlY10vDJJnAaxhvJ7QozKs5e2Jmq7dpN3agCgxPPrpT6zcH5Vw"
-
-// fetchfunction(apiName + apiEndpoint + apiKey)
-
-// async function fetchfunction(url)
-
-// console.log
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function() {
-
-// const promise = $.ajax({
-//     url: 'https://soccer.sportmonks.com/api/v2.0/players/search/'
-// });
-
-// promise.then(
-//     (data) => {
-//         console.log(data)
-//         render(data.results)
-//     },
-//     (error) => {
-//         console.error(error)
-//     }
-// })
+    setBoard(questions)
+})
